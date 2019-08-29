a7c0109a2da0d05aa1529d37b982d85279cbef12
dburns12 <dburns1@mail.usf.edu>
2019-08-29 16:22:48 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ISM4300MYFIRSTAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write line to print to screen
+            Console.WriteLine("This is my first Console App");
+
+            // Write line to print to screen
+            Console.WriteLine("I am learning how to publish to GitHub");
+        }
+    }
+}
